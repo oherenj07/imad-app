2fa1db2a99615d3554d2a7a22de62e4d30d41463
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2017-08-14 21:33:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile("Firts article is send");    
+  res.sendFile('Article one is visible');
 });
 
 app.get('/ui/style.css', function (req, res) {
