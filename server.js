483e86709d50dfc331542ddd672bd8df447c5ad0
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2017-09-09 11:12:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function (req, res) {
     res.send('Article one is fetched and is serving here.');
-}
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
