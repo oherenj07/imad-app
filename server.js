6ca9b25dde758a66a44659fdbd7cb6a5825e6c0a
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2017-09-09 11:13:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.get('/article-one', function (req, res) {
     res.send('Article one is fetched and is serving here.');
 });
 
+app.get('/article-two', function (req, res) {
+    res.send('Article 2 is fetched and is serving here.');
+});
+
+app.get('/article-three', function (req, res) {
+    res.send('Article 3 is fetched and is serving here.');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
