1cfd881bd47dd7e100d3a680edbf998a95e404d1
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-22 17:20:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function(req, res){
 });
 
 app.get('/article-two', function(req, res){
-   res.send("Article two requested and is served here.");
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function(req, res){
