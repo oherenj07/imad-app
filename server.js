98be32b691fd0e793bdc08cbbf5c6e3228e1ee95
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-23 20:30:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,27 +17,13 @@ function createTemplate(data){
     var date = data.date;
     var content = data.content;
     
-     var htmlTemplate = '<html>'+
-    '<head>'+
-        '<title>${title}</title>'+
-        '<meta name="viewport" content="width-device-width,initial-scale=1" />'+ 
-    '</head>'+
-    '<body>'+
-        '<div>'+
-            '<a href='/'>Home</a>'+
-        '</div>'+
-        '<hr/>'+
-        '<h3>${heading}</h3>'+
-        '<div>'+
-            '${date}'+
-        '</div>'+
-        '<div>'+
-            '${content}'
-        '</div>'+
-    '</body>'+
-
-'</html>'
-                                                                                                           ;
+     var htmlTemplate = `
+     
+     
+     
+     
+     `
+                                                                                                       ;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
