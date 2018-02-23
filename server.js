7f7ea1317c9cdb65d3ab230233dd522858f068be
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-23 19:43:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,31 +13,27 @@ var content = {
 };
 function createTemplate(data){
     
-     var htmlTemplate = ' \
-    <html>\
-        <head>\
-            <title>${title}</title>\
-            <meta name="viewport" content="width-device-width,initial-scale=1" /> \
-        </head>\
-        <body>\
-            <div>\
-                <a href='/'>Home</a>\
-            </div>\
-            <hr/>\
-            <h3>${heading}</h3>\
-            <div>\
-                ${date}\
-            </div>\
-            <div>\
-                ${content}'+          
-            '</div>\
-        </body>\
-    \
-    </html>\
-    \
-    \
-    \
-    ';
+     var htmlTemplate = ' '+
+   ' <html>'+
+        '<head>'+
+            '<title>${title}</title>'+
+            '<meta name="viewport" content="width-device-width,initial-scale=1" />'+
+        '</head>'+
+        '<body>'+
+            '<div>'+
+                '<a href='/'>Home</a>'+
+            '</div>'+
+            '<hr/>'+
+            '<h3>${heading}</h3>'+
+            '<div>'+
+                '${date}'+
+            '</div>'+
+            '<div>'+
+                '${content}'+          
+            '</div>'+
+        '</body>'+
+    '</html>'
+    +'';
     
     
 }
