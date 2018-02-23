82493a686f5fa11350076811b88fe11f0d4bb719
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-23 21:11:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articleName',function(req, res){
-    var articleName = req.params.articleName;
+    var articleName = req.param.articleName;
    res.send(createTemplate(articles[articleName]));
 });
 
