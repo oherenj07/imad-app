e2b0cc6caf63b7716e712b04c9924408cdf039e5
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-24 19:04:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text');
-    element.innerHTML = 'New Value';
+var element = document.getElementById("main-text");
+    element.innerHTML = "New Value";
