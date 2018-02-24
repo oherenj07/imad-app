60f76740b4c494bd834e641b4afdcb862667c16e
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-24 20:31:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,13 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
+button.onclick = function(){
+ //Make a request tothe counter end point
 
-var img = document.getElementById('madi');
+//Capture the response and store it in a variable.
 
-img.onclick = function(){
-    var marginLeft = 0;
-  function moveRight(){
-      marginLeft = marginLeft + 10;
-      img.style.marginLeft = marginLeft +'px';
-  }
-  var interval = setInterval(moveRight,100);
+//Render the variable in the correct span
+counter = counter + 1;
+var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
