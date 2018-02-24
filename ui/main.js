4f8499ac3de3bbf92abc9d420ebf18583dd2b094
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-24 19:16:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,14 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+var img = document.getElementById('madi');
+
+img.onclick = function(){
+    var marginLeft = 0;
+  function moveRight(){
+      marginLeft = marginLeft + 10;
+      img.style.marginLeft = marginLeft +'px';
+  }
+  var interval = setInterval(moveRight,100);
+};
