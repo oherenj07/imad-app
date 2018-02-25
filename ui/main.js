18afbb788855d69eb3245241a06c37990b6da49a
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-25 15:18:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,8 +32,8 @@ var submit = document.getElementById('submit_btn');
         //capture the list of names and render it a list
         var names = ['name1','name2','name3','name4'];
         var list = '';
-        for(var i=0;i<names.Length;i++){
-            list += '<li>'+ names[i]+'</li>';
+        for(var i=0;i<names.length;i++){
+            list += '<li>'+ names[i] +'</li>';
         }
         var ul =document.getElementById('namelist');
             ul.innerHTML = list;
