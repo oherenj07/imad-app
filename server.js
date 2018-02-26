4920897b60cf435ca11967b7998ec64b22035cc0
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-02-26 18:34:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,9 +106,21 @@ app.get('/test-db', function(req, res){
     });
 });
 
-app.get('/:articleName',function(req, res){
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+app.get('/article/:articleName',function(req, res){
+    
+    pool.query("SELECT * FROM article where title ="+req.params.articleName, function(err, params){
+     if(err){
+         res.status(500).send(err.toString());
+     }else
+        if(result.length === 0){
+            res.status(404).send('Article not found yet');
+        }else{
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+        
+    });
+   
 });
 
 
