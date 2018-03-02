f9a5c442f0000b77b3e2b91d005d84444bb96bf8
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-03-02 19:05:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
    user:'oherenj',
@@ -91,6 +92,17 @@ app.get('/submit-name', function(req, res){ //Url: /submit-name?name=xxxx
    res.send(JSON.stringify(names));//TODO
 });
 
+
+function hash(input,salt){
+    //How do we create hash?
+    var hashed = crypto.pbkdf2Sync('input','salt',512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+});
 
 var pool = new Pool(config); 
 app.get('/test-db', function(req, res){
