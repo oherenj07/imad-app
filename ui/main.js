b24efc8c91ac28614997ceb8d47d5a74b0b11885
oherenj07 <30774680+oherenj07@users.noreply.github.com>
2018-03-03 11:28:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,13 +47,43 @@ button.onclick = function(){
         //Not done yet
         
     };
-//Make the request
+    //Make the Get request 
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 request.open('GET', 'http://oherenj.imad.hasura-app.io/submit-name?name='+ name,true);
 request.send(null);
 };
+    
+    
+        var submit = document.getElementById('submit_button');
+    submit.onclick = function(){
+        //Create a request object
+     var request = new XMLHttpRequest();
+ 
+//Capture the response and store it in a variable.
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+             console.log("user logged in");
+        }
+        var ul =document.getElementById('namelist');
+            ul.innerHTML = list;
+            }
+        }
+        //Not done yet
+        
 
+
+//Make Post request
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+console.log("username");
+console.log("password");
+request.open('GET', 'http://oherenj.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username: username, password: password}));
+};
 
         
        
